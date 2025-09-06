70873a5dbb655d297110dbc24118f0ff90985464
japneetJava001 <japneet.java@gmail.com>
2025-09-06 22:58:30 +0530

    Time: 480 ms (60.36%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+SELECT
+    today.id as Id
+FROM Weather yesterday
+CROSS JOIN Weather today
+WHERE DATEDIFF(today.recordDate, yesterday.recordDate) = 1
+    AND today.temperature > yesterday.temperature;
