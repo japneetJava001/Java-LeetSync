811b64f4363df90747fc87dc81c0a4011be8853e
japneetJava001 <japneet.java@gmail.com>
2025-09-07 15:59:20 +0530

    Time: 583 ms (89.51%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+SELECT 
+    e1.employee_id,
+    e1.name,
+    COUNT(e2.employee_id) AS reports_count,
+    ROUND(AVG(e2.age), 0) AS average_age
+FROM Employees e1
+CROSS JOIN Employees e2 ON e2.reports_to = e1.employee_id
+GROUP BY e1.employee_id
+ORDER BY e1.employee_id;
