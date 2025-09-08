38675352e95a22d1ca7a7e6c51dd39f6106c7e3a
japneetJava001 <japneet.java@gmail.com>
2025-09-08 12:38:59 +0530

    Time: 298 ms (99.52%) | Memory: 0B (100.00%) - LeetSync

---
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+SELECT
+    e1.name
+FROM Employee e1
+CROSS JOIN Employee e2 ON e2.managerId = e1.id
+GROUP BY e2.managerId
+HAVING COUNT(e2.managerId) >= 5;
