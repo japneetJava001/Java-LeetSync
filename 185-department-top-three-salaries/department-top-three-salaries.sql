38ee88ee7cb4fd01c31188fb839a1be3ff8873fe
japneetJava001 <japneet.java@gmail.com>
2025-09-11 11:26:48 +0530

    Time: 1041 ms (44.88%) | Memory: 0B (100.00%) - LeetSync

---
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+
+WITH ranked_salary AS (
+    SELECT
+        d.name AS Department,
+        e.name AS Employee,
+        e.salary AS Salary,
+        DENSE_RANK() OVER (PARTITION BY e.departmentId ORDER BY e.salary DESC) AS Salary_Rank
+    FROM Employee e
+    LEFT JOIN Department d ON d.id = e.departmentId
+)
+
+SELECT
+    Department,
+    Employee,
+    Salary
+FROM ranked_salary
+WHERE Salary_Rank <= 3
