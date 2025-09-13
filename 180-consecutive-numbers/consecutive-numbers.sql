14a9b97f3e14bd81ea402f5080c3163c3f576d43
japneetJava001 <japneet.java@gmail.com>
2025-09-13 22:25:20 +0530

    Time: 522 ms (77.88%) | Memory: 0B (100.00%) - LeetSync

---
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+SELECT 
+    DISTINCT num AS ConsecutiveNums
+FROM (
+    SELECT 
+        num,
+        a.id - ROW_NUMBER() OVER (PARTITION BY num ORDER BY id) AS label
+    FROM Logs a
+    ) cte
+GROUP BY label, num
+HAVING COUNT(num) >= 3;
